fe8db2d71cc220037869f88d3b446b38f9279ba8
nitesh kumar singh <nkscoder@gmail.com>
2019-03-30 15:21:10 +0530

    Django’s MVC, URLs, and View Functions

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-home_page = None
+# home_page = None
+
+# this funcation working
+# def home_page():
+#     pass
+
+def home_page():
+    return HttpResponse('<html><title>To-Do lists</title></html>')
